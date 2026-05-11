14dc8e57e42935eceea6e090ae15c039a1df2467
Matthew Ingram <moomie679@gmail.com>
2026-05-12 00:54:01 +0100

    Gallery API v1

--- API/api.py
+++ API/api.py
@@ -1,25 +1,38 @@
 
 
 
-
-
-
 import quart 
 import aiohttp
 
 app = quart.Quart(__name__)
 
-ALBUM_ID = "e9ac43a4-c934-4836-8369-c8ad6087f120?at=8cd0f5c8-d007-4b91-9c57-dde68b147f5d"
+ALBUM_ID = "742c8e97-1a4a-4bbe-bc52-4a350855ae36"
 GALLERY_ROOT = "https://photos.matprojects.xyz"
+ARGS = "?slug=website&withoutAssets=false"
 
 @app.route('/api/gallery')
 async def _gallery():
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'{GALLERY_ROOT}/api/albums/{ALBUM_ID}') as resp:
+        async with session.get(f'{GALLERY_ROOT}/api/albums/{ALBUM_ID}{ARGS}') as resp:
             data = await resp.json()
             return data
 
+@app.route('/api/gallery/<photo_id>/<size>')
+async def _gallery_photo(photo_id, size):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'{GALLERY_ROOT}/api/assets/{photo_id}/thumbnail{ARGS}&size={size}') as resp:
+
+            
+            status = resp.status 
+            
+            # 2. content equivalent (MUST be awaited)
+            content = await resp.read() 
+            
+            # 3. headers.items() equivalent
+            headers = resp.headers.items()
+            
+            return content, status, headers
 
 
 app.run(host='0.0.0.0', port=8000)
